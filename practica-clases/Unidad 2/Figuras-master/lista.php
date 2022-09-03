ec69f58c572c4e7bbe47934f3cedd1094ce90826
Martina Sanz <msanzcaparros@gmail.com>
2022-09-03 17:25:01 -0300

    Unidad 2 figuras sin ruteo

--- practica-clases/Unidad 2/Figuras-master/lista.php
+++ practica-clases/Unidad 2/Figuras-master/lista.php
@@ -1,22 +1,20 @@
 <?php
-require_once 'lib/Figuras.php';
+    require_once 'lib/Figuras.php';
 
-// instancia la clase Figuras para acceder a las figuras
-$figuras = new Figuras();
+    // instancia la clase Figuras para acceder a las figuras
+    $figuras = new Figuras();
 
-echo 
-"<h1>Listado de figuras</h1>
-    <ul>";
+    echo 
+    "<h1>Listado de figuras</h1>
+        <ul>";
 
-foreach($figuras->getAll() as $figura) {
-    echo "<li>" . 
-            $figura->ToString() . 
-            " | <a href='verFigura.php?id=" . $figura->getId() . "'>VER </a>" .
-         "</li>";
-}
+    foreach($figuras->getAll() as $figura) {
+        echo "<li>" . 
+                $figura->ToString() . 
+                " | <a href='verFigura.php?id=" . $figura->getId() . "'>VER </a>" .
+            "</li>";
+    }
 
-echo "
-    </ul>
-    <a href='./'>Volver</a>";
-
-?>
+    echo "
+        </ul>
+        <a href='./'>Volver</a>";
